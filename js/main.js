a8b7e488f24a7c563770ecf781353c2f37569c91
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-03-02 18:28:42 +0100

    4-rel

--- js/main.js
+++ js/main.js
@@ -12,26 +12,49 @@ $(document).ready(function () {
 
     function nextSlide () {
         if ($(".slideshow img.active").hasClass("last")) {
+
             $(".slideshow img.last").removeClass("active");
-            $(".slideshow img.first").addClass("active")
+            $(".slideshow img.first").addClass("active");
+
+            // bullet
+
+            $(".bullet-slide i.last").removeClass("active");
+            $(".bullet-slide i.first").addClass("active");
         } else {
             var slideActive = $(".slideshow img.active");
             var slideNext = $(".slideshow img.active").next();
+            // bullet
+            var bulletActive = $(".bullet-slide i.active");
+            var bulletNext = $(".bullet-slide i.active").next();
 
             slideActive.removeClass("active");
             slideNext.addClass("active");
+            // bullet
+            bulletActive.removeClass("active");
+            bulletNext.addClass("active");
         }
     }
     function lastSlide () {
         if ($(".slideshow img.active").hasClass("first")) {
+
             $(".slideshow img.first").removeClass("active");
-            $(".slideshow img.last").addClass("active")
+            $(".slideshow img.last").addClass("active");
+            //bullet
+            $(".bullet-slide i.first").removeClass("active");
+            $(".bullet-slide i.last").addClass("active");
+
         } else {
             var reverseSlideActive = $(".slideshow img.active");
             var reverseSlideLast = $(".slideshow img.active").prev();
+            //bullet
+            var reverseBulletActive = $(".bullet-slide i.active");
+            var reverseBulletNext = $(".bullet-slide i.active").prev();
 
             reverseSlideActive.removeClass("active");
             reverseSlideLast.addClass("active");
+            //bullet
+            reverseBulletActive.removeClass("active");
+            reverseBulletNext.addClass("active");
         }
     }
 
